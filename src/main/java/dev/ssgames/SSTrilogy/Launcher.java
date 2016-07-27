72c7d1d0de89f24de0010ce58b11fd6bd7300a6b
shiv4896 <shivshnkr420@gmail.com>
2016-07-27 22:03:28 +0530

    launcher java

---
+++ src/main/java/dev/ssgames/SSTrilogy/Launcher.java
@@ -0,0 +1,11 @@
+package dev.ssgames.SSTrilogy;
+
+
+public class Launcher {
+
+    public static void main(String []args){
+
+        Login Game=new Login("SSTrilogy",900,600) ;
+        Game.init();
+    }
+}
